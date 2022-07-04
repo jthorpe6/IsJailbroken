590cee17e714ec5049de25be9e8605d9ef76cf90
Joe Thorpe <joethorpe6@protonmail.com>
2022-07-04 20:13:09 +0100

    Initial Commit

---
+++ jailbroken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jailbroken
+//
+//  Created by Joe Thorpe on 04/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jailbroken/jailbrokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jailbrokenApp.swift
+//  jailbroken
+//
+//  Created by Joe Thorpe on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct jailbrokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
